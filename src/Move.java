d6c45fdbe7782df38065387342ac8674f8ffe920
Phillip Le <phillip.huy.le@gmail.com>
2021-01-26 14:05:59 +1000

    Implemented Move

--- src/Move.java
+++ src/Move.java
@@ -1,2 +1,26 @@
 public class Move {
+    // The starting and ending positions of the move
+    private final Position src;
+    private final Position dst;
+
+    // The player who made the move
+    private final Player player;
+
+    public Move(Player player, Position src, Position dst) {
+        this.player = player;
+        this.src = src;
+        this.dst = dst;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public Position getSrc() {
+        return this.src;
+    }
+
+    public Position getDst() {
+        return this.dst;
+    }
 }
